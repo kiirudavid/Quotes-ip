eca6246b2bc0527f18fb7f19c512b8c93c65abc7
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-22 23:00:44 +0300

    created components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quotes';
 }
+
+
+
+
+
+// SAMPLE BELOW
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   goals:string[];
+
+//   constructor(){
+//     this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+//   } 
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TheQuoteComponent } from './the-quote/the-quote.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheQuoteComponent,
+    DateCountPipe,
+    HighlightDirective,
+    DetailsComponent,
+    FormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
