0a2583d6ab9242d1e6fa8b2e874950d6f47c9682
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-23 20:05:44 +0300

    modified directives typescript

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -1,5 +1,3 @@
-import { HighlightDirective } from './highlight.directive';
-
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
     const directive = new HighlightDirective();
