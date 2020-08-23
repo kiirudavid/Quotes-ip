0761e182912f4c16900634e9d600730672cd79e8
davidmaina085@gmail.com <davidmaina085@gmail.com>
2020-08-23 20:08:57 +0300

    exporting class quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,12 +2,12 @@
 // }
 
 export class Quote {
-    public upvotes:number
-    public downvotes:number
-    public completeDate:Date
-    constructor(public name:string, public author:string,public quote:string){
-      this.upvotes =0;
-      this.downvotes =0;
-      this.completeDate=new Date;
-    }
+  public upvotes:number
+  public downvotes:number
+  public completeDate:Date
+  constructor(public name:string, public author:string,public quote:string){
+    this.upvotes =0;
+    this.downvotes =0;
+    this.completeDate=new Date;
   }
+}
